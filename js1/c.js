a35c17f730b9ea83e66fbcbbdca37f99044cce56
ThotaUsha <135428498+ThotaUsha@users.noreply.github.com>
2026-07-08 09:42:50 +0530

    commit

---
+++ js1/c.js
@@ -0,0 +1,11 @@
+var msg;
+msg = "<p><code>The actual script is in external script file called common.js</code></p>";
+
+function addNos(headVar,bodyVar)
+{
+document.write(msg);
+var sum=headVar+bodyVar;
+document.write("The sum of the variables headVar and bodyVar is <b>" + sum +"</b>");
+}
+
+
